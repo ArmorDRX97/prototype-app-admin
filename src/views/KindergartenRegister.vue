49c47721cf09af7e0a4fc5b7637d0c63120e7ea4
timur <armor.d.r.x@gmail.com>
2023-01-19 19:43:21 +0600

    first commit

---
+++ src/views/KindergartenRegister.vue
@@ -0,0 +1,33 @@
+<template>
+	<h4>Регистрация учреждения</h4>
+	<hr>
+	<div class="row">
+		<div class="col-12 col-md-4">
+			<div class="form-group mb-3">
+				<label class="form-label">Выберите детский сад</label>
+				<select class="form-control" v-model="garden">
+					<option v-for="garden in gardenList" :key="garden">{{garden}}</option>
+				</select>
+			</div>
+		</div>
+		{{garden}}
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'Setting',
+		data(){
+			return{
+				gardenList: [
+					'Шабыт, детский сад №95',
+					'Сункар, детский сад №21',
+					'Алижан Хаматов, детский сад №44',
+					'Керемет, детский сад №23',
+					'Казакамаанаа, детский сад №77',
+				],
+				garden: null
+			}
+		}
+	}
+</script>
---
+++ src/views/Login.vue
@@ -0,0 +1,80 @@
+<template>
+	<div class="log-reg-container">
+		<div class="content">
+			<h4>Регистрация</h4>
+			<hr>
+			<div class="row">
+				<div class="col-12 col-md-6">
+					<div class="form-group mb-3">
+						<label class="form-label">Имя</label>
+						<input type="text" placeholder="Введите Ваше имя" class="form-control">
+					</div>
+				</div>
+				<div class="col-12 col-md-6">
+					<div class="form-group mb-3">
+						<label class="form-label">Фамилия</label>
+						<input type="text" placeholder="Введите Свою фамилию" class="form-control">
+					</div>
+				</div>
+				<div class="col-12 col-md-6">
+					<div class="form-group mb-3">
+						<label class="form-label">Отчетсво</label>
+						<input type="text" placeholder="Введите Ваше отчетсво" class="form-control">
+					</div>
+				</div>
+				<div class="col-12 col-md-6">
+					<div class="form-group mb-3">
+						<label class="form-label">ИИН</label>
+						<input type="number" placeholder="Введите Ваш ИИН" class="form-control">
+					</div>
+				</div>
+				<div class="col-12 col-md-6">
+					<div class="form-group mb-3">
+						<label class="form-label">Email</label>
+						<input type="email" placeholder="Введите Вашу почту" class="form-control">
+					</div>
+				</div>
+				<div class="col-12 col-md-6">
+					<div class="form-group mb-3">
+						<label class="form-label">Телефон</label>
+						<input type="number" placeholder="Ваш номер телефона" class="form-control">
+					</div>
+				</div>
+				<div class="col-12 col-md-6">
+					<div class="form-group mb-3">
+						<label class="form-label">Пароль</label>
+						<input type="password" placeholder="Ваш пароль" class="form-control">
+					</div>
+				</div>
+				<div class="col-12 col-md-6">
+					<div class="form-group mb-3">
+						<label class="form-label">Повторите пароль</label>
+						<input type="password" placeholder="Повторите пароль" class="form-control">
+					</div>
+				</div>
+			</div>
+			<hr>
+			<div class="form-check">
+				<input class="form-check-input" type="checkbox" value="" id="check1">
+				<label class="form-check-label" for="check1">
+					Я соглашаюсь с <a href="#!">условиями использования сервиса</a>
+				</label>
+			</div>
+			<div class="form-check">
+				<input class="form-check-input" type="checkbox" value="" id="check2">
+				<label class="form-check-label" for="check2">
+					Еще какой-нибудь чекбокс с условиями
+				</label>
+			</div>
+			<hr>
+			<router-link to="/" class="btn btn-light">Зарегистрироваться</router-link>
+		</div>
+	</div>
+</template>
+
+
+<script>
+	export default {
+
+	}
+</script>
